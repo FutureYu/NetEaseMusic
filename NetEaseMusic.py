eefe34ab81ebfc05c70338176aa75821ad583676
FanYu <32595791+FutureYu@users.noreply.github.com>
2018-12-04 16:21:43 +0800

    修复userid的bug

--- NetEaseMusic.py
+++ NetEaseMusic.py
@@ -10,11 +10,14 @@ print()
 while True:
     oldUrl = input("请复制网易云歌曲地址，如：https://music.163.com/#/song?id=413812473\n地址：") #https://music.163.com/#/song?id=228252
     print()
-    songIds = re.findall("id=(.*[0-9])",oldUrl)
+    if "&" in oldUrl:
+        songIds = re.findall("id=(.*[0-9])&",oldUrl)
+    else:
+        songIds = re.findall("id=(.*[0-9])",oldUrl)
     songId = songIds[0]
     nameUrl = f"http://music.163.com/api/song/detail/?id={songId}&ids=%5B{songId}%5D"
     headers = {
-        'Cookie': '''__f_=1535714748410; _ntes_nnid=b71ffe36e46af75f1d79299195dcb1fb,1535714747971; _ntes_nuid=b71ffe36e46af75f1d79299195dcb1fb; P_INFO=13513673164|1536330039|0|ydaq_web|00&99|null&null&null#jis&null#10#0#0|&0|null|13513673164; _iuqxldmzr_=32; WM_TID=SCg4H5LsBItBVVFAVVN8aVNQaPCVEvuf; vinfo_n_f_l_n3=a2a7dd60a756b516.1.7.1536064453105.1539087711502.1539687814569; __utmc=94650624; __utmz=94650624.1539775406.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%E7%BD%91%E6%98%93%E4%BA%91; playerid=98072337; __utma=94650624.952492931.1537873835.1539777821.1539781919.4; WM_NI=rpHMEY1e%2B4XArYAn0f%2F7c9z9sZKzvx4CmCfYGDdvQXflqu6rsGAI8tyBx4EDIVr4J0qgt9C2iqOtOheCTDVOV%2FiXn44c3Bz8uuNs6zbPBboUr0NYbzij1C3t5mxDajuwdnc%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee8ce579b09f8ca4dc50a39e8fb6c84a838b9aaab7488f9f86aad63af2f5a7acc52af0fea7c3b92ab2b088aec853f3bbe5d5db4bb2878f8fb36eede9addaf933b186859bf574f89da3a4e553859cac97d454a6b4bf96b57ab1998faabb80bb9eb8d5c8508b8abed5c93cb3b9bcdac23aabaf8195ce64a79d9ca5d54596f1a7b9ed40f8bdfda3f934b7ab8294b572a2908e83ee5fb2f082bae172abb6ffa4b280f7b3b8a5e462b7a9ac8fe237e2a3; JSESSIONID-WYYY=J10kl%2Bq729ZO8mGGU7GO4M54Pb1C0ePi8HflIg9%5CUuag8AU5y2Ym6fM3VTvKJJ%2FAV0DM3yh7A%2BHFb2B8HWUbMoK%2FcktuQRrMvwXjMq1yFPeRzxj8IrwdCo930p8xI%5CQgttbqUMP9tfzbveeYEFQyTJPCS4Of5CFPq%5CEERCaa1xd9Xnnu%3A1539787650118; __utmb=94650624.36.10.1539781919''',
+        'Cookie': '''_ntes_nnid=b71ffe36e46af75f1d79299195dcb1fb,1535714747971; _ntes_nuid=b71ffe36e46af75f1d79299195dcb1fb; _iuqxldmzr_=32; WM_TID=SCg4H5LsBItBVVFAVVN8aVNQaPCVEvuf; __utmz=94650624.1540643279.10.5.utmcsr=cnblogs.com|utmccn=(referral)|utmcmd=referral|utmcct=/; P_INFO=discretemath16@126.com|1540713894|0|other|00&99|jis&1540690871&mail126#jis&320100#10#0#0|&0|urs&mail126|discretemath16@126.com; mail_psc_fingerprint=1964e5031818834ef725f65ba0cae890; vjuids=1a9b40d72.166dc2f95e1.0.54d6d59e1d308; vinfo_n_f_l_n3=a2a7dd60a756b516.1.11.1536064453105.1539960122601.1541292440311; vjlast=1541292398.1542459367.13; __f_=1543742942721; JSESSIONID-WYYY=HxXcHXnREmjb9qfSbID9VoEJrEWkrZ%2BZhK%2F7iUzrSIg1CaX3nJVAHtS3qPHtZ%5CERhyE%2FJxhRiaNpA7Qov%5Ci9WMRz%5CtNzNDkZNij6Gal%2BiJpb05ZcFvuI2Vt7b4k%2Bsi9F2%5C3Mj7EnlPzqaAuj1GhChyNwlG8s5UhiXNf3qSgov4MeKloB%3A1543909236540; __utma=94650624.952492931.1537873835.1540643279.1543907437.11; __utmc=94650624; WM_NI=VBMWIdpTsOvrSus0Al63z7rdV5MuOaR4zgvpL%2FdewFf88pWwoBULOnzUU3N%2BJ7DF0zDUMBVZsH4Vhm%2FSYgi4%2FmTCT4aDzuU8ufqryOtKgZal84rv1Ua1m4md%2FIOCjZmmNEM%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee87ea4792b0b88af053fc928fb2c14b868e8ebbbc6abbaa8ab6ca45f8f5f8b7aa2af0fea7c3b92a969e83b8cc70b48cbdb0f57a91abbdd1c468f6edb6d8f17096eb8396ce6b92ecb688f57c90f1ac86f64ea6e8f891d67dad9e9badce66ba8b9bd4cd218ae98ca7b372888b83a3d552b198fcb7eb67ed8d9788ec40edb39ca4d740a39cb696d834b7babf93fc45b895a9bbe468fcecbe97b45fa3efa3d5f070ed9781a4e952afb382a6d037e2a3; __utmb=94650624.9.10.1543907437''',
         'User-Agent': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'''
     }
 
@@ -27,13 +30,17 @@ while True:
     print()
     print(f"已检测到歌曲：{name}-{artist}")
     newUrl = f"http://music.163.com/song/media/outer/url?id={songId}.mp3"  #http://music.163.com/song/media/outer/url?id=228252.mp3
-    songHtml = requests.get(newUrl)
+    songHtml = requests.get(newUrl,headers = headers,allow_redirects=False)
 
-    if songHtml.url == "https://music.163.com/404":
-        print("下载失败，无法下载！")
+    if songHtml.url == "https://music.163.com/404" or songHtml.url == "http://music.163.com/404":
+        print("下载失败，无法下载！请检查歌曲是否需付费")
     else:
         print("正在下载...")
-        urllib.request.urlretrieve(newUrl, f"{name}-{artist}.mp3")
-        print("下载完成！")
+        newNewUrl = songHtml.headers['location']
+        if newNewUrl == "https://music.163.com/404" or newNewUrl == "http://music.163.com/404":
+            print("下载失败，无法下载！请检查歌曲是否需付费")
+        else:
+            urllib.request.urlretrieve(newNewUrl, f"{name}-{artist}.mp3")
+            print("下载完成！")
     print()
     print()
